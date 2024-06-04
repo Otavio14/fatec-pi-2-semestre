cc5b08db54fab8d7a547d1fc3ad2926c8c08a41a
Vinij77 <161947832+Vinij77@users.noreply.github.com>
2024-06-04 20:49:10 -0300

    Update usuario.validator.js

--- backend/src/validators/usuario.validator.js
+++ backend/src/validators/usuario.validator.js
@@ -3,16 +3,16 @@ import { body, param } from "express-validator";
 export const createUserValidator = [
   body("email").isEmail().withMessage("Email inválido"),
   body("name").isString().withMessage("Nome inválido"),
-  body('age').isInt().withMessage("Idade inválida").optional(),
+  body('age').isInt().withMessage("Idade inválida").optional()
 ];
 
 export const updateUserValidator = [
   param("id").isInt().withMessage("ID inválido"),
   body("email").isEmail().withMessage("Email inválido"),
   body("name").isString().withMessage("Nome inválido"),
-  body('age').isInt().withMessage("Idade inválida").optional(),
+  body('age').isInt().withMessage("Idade inválida").optional()
 ];
 
 export const deleteUserValidator = [
-  param("id").isInt().withMessage("Id inválido"),
+  param("id").isInt().withMessage("Id inválido")
 ];
