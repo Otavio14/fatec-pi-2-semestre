27ac900d3a855d3befb682dcf51eb6db825f1d21
afbio87 <161135917+afbio87@users.noreply.github.com>
2024-06-07 18:55:32 -0300

    Criação do código produtos_pedidos.route.js

---
+++ backend/src/routes/produtos_pedidos.route.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import Produtos_PedidosController from "../controllers/usuarios.controller.js";
+import {
+  createProdutos_PedidosValidator,
+  updateProdutos_PedidosValidator,
+  deleteProdutos_PedidosValidator,
+} from "../validators/produtos_pedidos.validator.js";
+
+const router = Router();
+
+router.get("/", Produtos_PedidosController.index);
+router.get("/:id", Produtos_PedidosController.show);
+router.post("/", createProdutos_Pedidosalidator, Produtos_PedidosController.create);
+router.put("/:id", updateProdutos_PedidosValidator, Produtos_PedidosController.update);
+router.delete("/:id", deleteProdutos_PedidosValidator, Produtos_PedidosController.delete);
+
+export default router;
