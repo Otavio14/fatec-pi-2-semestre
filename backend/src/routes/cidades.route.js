bdc6f2e41b16490f1ad4b39c6a4eaa97098a3bb8
afbio87 <161135917+afbio87@users.noreply.github.com>
2024-06-08 08:39:12 -0300

    Criação do código cidades.route.js

---
+++ backend/src/routes/cidades.route.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import CidadesController from "../controllers/cidades.controller.js";
+import {
+  createCidadesValidator,
+  updateCidadesValidator,
+  deleteCidadesValidator,
+} from "../validators/cidades.validator.js";
+
+const router = Router();
+
+router.get("/", CidadesController.index);
+router.get("/:id", CidadesController.show);
+router.post("/", createCidadesValidator, CidadesController.create);
+router.put("/:id", updateCidadesValidator, CidadesController.update);
+router.delete("/:id", deleteCidadesValidator, CidadesController.delete);
+
+export default router;
