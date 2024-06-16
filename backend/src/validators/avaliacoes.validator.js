488282d74e0e3962edcfb2b99063f59aa72294ad
Bruno Johann <88054915+brunojohannn@users.noreply.github.com>
2024-06-16 20:33:43 -0300

    commit correção validator

--- backend/src/validators/avaliacoes.validator.js
+++ backend/src/validators/avaliacoes.validator.js
@@ -13,11 +13,11 @@ export const createAvaliacoesValidator = [
       }
     }
   */
-  body("produto").isInt().withMessage("Produto inválido"),
-  body("cliente").isInt().withMessage("Cliente inválido"),
+  body("id_produtos").isInt().withMessage("Produto inválido"),
+  body("id_clientes").isInt().withMessage("Cliente inválido"),
   body("nota").isInt().withMessage("Nota inválida"),
   body("comentario").isString().withMessage("Comentário inválido"),
-  body("dtAvaliacao").isString().withMessage("Data inválida"),
+  body("dt_avaliacao").isString().withMessage("Data inválida"),
 ];
 
 export const updateAvaliacoesValidator = [
@@ -34,11 +34,11 @@ export const updateAvaliacoesValidator = [
     }
   */
   param("id").isInt().withMessage("ID inválido"),
-  body("produto").isInt().withMessage("Produto inválido"),
-  body("cliente").isInt().withMessage("Cliente inválido"),
+  body("id_produtos").isInt().withMessage("Produto inválido"),
+  body("id_clientes").isInt().withMessage("Cliente inválido"),
   body("nota").isInt().withMessage("Nota inválida"),
   body("comentario").isString().withMessage("Comentário inválido"),
-  body("dtAvaliacao").isString().withMessage("Data inválida"),
+  body("dt_avaliacao").isString().withMessage("Data inválida"),
 ];
 
 export const deleteAvaliacoesValidator = [
--- backend/src/validators/cidades.validator.js
+++ backend/src/validators/cidades.validator.js
@@ -14,7 +14,7 @@ export const createCidadesValidator = [
     }
   */
   body('nome').isString().withMessage("Nome inválido"),
-  body('estado').isInt().withMessage("Estado inválido")
+  body('id_estados').isInt().withMessage("Estado inválido")
 ];
 
 export const updateCidadesValidator = [
@@ -32,7 +32,7 @@ export const updateCidadesValidator = [
   */
   param("id").isInt().withMessage("ID inválido"),
   body('nome').isString().withMessage("Nome inválido"),
-  body('estado').isInt().withMessage("Estado inválido")
+  body('id_estados').isInt().withMessage("Estado inválido")
 ];
 
 export const deleteCidadesValidator = [
