78a3cf939807aa4003815d12e90903ad18f37995
OtavioBScar <otavio.scarpelline@gmail.com>
2024-06-18 14:35:01 -0300

    mudança logo da aba e nome

--- frontend/src/shared/api.js
+++ frontend/src/shared/api.js
@@ -4,4 +4,5 @@ export const api = axios.create({
   baseURL: window.location.hostname.includes("localhost")
     ? "http://localhost:3000/api/"
     : "https://fatec-pi-2-semestre.onrender.com/api/",
+    // : "http://localhost:3000/api/",
 });
