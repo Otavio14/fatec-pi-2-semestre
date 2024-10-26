28a08d0522a06f65998811ec2da3ca82d56104f1
Bruno Johann <88054915+brunojohannn@users.noreply.github.com>
2024-10-26 22:43:39 +0000

    4 Models (sem chave estrangeira)

---
+++ backend-java/src/main/java/com/fobov/fobov/model/Cidades.java
@@ -0,0 +1,26 @@
+package com.fobov.fobov.model;
+
+public class Cidade {
+    private int id_cidade;
+    private String nome;
+    private int id_estado;
+
+    // ID
+    public int getId() {
+        return id_cidade;
+    }
+
+    public void setId(int id_cidade) {
+        this.id_cidade = id_cidade;
+    }
+
+    // NOME
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
