47c24823ceca0ad467b8fc86ca5aeb145dbe6f08
Bruno Johann <88054915+brunojohannn@users.noreply.github.com>
2024-10-29 01:13:22 +0000

    Correção ClientesCupons model

--- backend-java/src/main/java/com/fobov/fobov/model/ClientesCupons.java
+++ backend-java/src/main/java/com/fobov/fobov/model/ClientesCupons.java
@@ -5,6 +5,8 @@ import java.util.Date;
 public class ClientesCupons {
     private int id;
     private Date data_utilizacao;
+    private int id_clientes;
+    private int id_cupons;
 
     // ID
     public int getId() {
@@ -23,4 +25,22 @@ public class ClientesCupons {
     public void setDataUtilizacao(Date data_utilizacao) {
         this.data_utilizacao = data_utilizacao;
     }
+
+    // CHAVE ESTRANGEIRA id_clientes
+    public int getIdCidades(){
+        return id_clientes;
+    }
+    public void setIdCidades(int id_clientes){
+        this.id_clientes = id_clientes;
+    }
+
+    // CHAVE ESTRANGEIRA id_cupons
+    public int getIdCidades(){
+        return id_cupons;
+    }
+    public void setIdCidades(int id_cupons){
+        this.id_cupons = id_cupons;
+    }
+
+    
 }
