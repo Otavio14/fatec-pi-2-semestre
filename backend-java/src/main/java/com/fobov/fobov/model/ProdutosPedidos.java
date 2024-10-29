dfd9b6232686e8e0d5c22cf839751b16befadfb2
Bruno Johann <88054915+brunojohannn@users.noreply.github.com>
2024-10-29 01:25:17 +0000

    Correção ProdutosPedidos Model

--- backend-java/src/main/java/com/fobov/fobov/model/ProdutosPedidos.java
+++ backend-java/src/main/java/com/fobov/fobov/model/ProdutosPedidos.java
@@ -4,6 +4,8 @@ public class ProdutosPedidos {
     private int id;
     private double preco;
     private int quantidade;
+    private int id_produtos;
+    private int id_pedidos;
 
     // ID
     public int getId() {
@@ -30,5 +32,21 @@ public class ProdutosPedidos {
 
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
+    }
+
+    // CHAVE ESTRANGEIRA id_produtos
+    public int getIdCidades(){
+        return id_produtos;
+    }
+    public void setIdCidades(int id_produtos){
+        this.id_produtos = id_produtos;
+    }
+
+    // CHAVE ESTRANGEIRA id_pedidos
+    public int getIdCidades(){
+        return id_pedidos;
+    }
+    public void setIdCidades(int id_pedidos){
+        this.id_pedidos = id_pedidos;
     }
 }
