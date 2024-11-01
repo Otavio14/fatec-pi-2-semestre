2027c780f5baa949051810ff394294287ac17969
OtavioBScar <otavio.scarpelline@gmail.com>
2024-10-31 21:17:32 -0300

    fix: ajuste no swagger

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import routes from "./src/routes/index.js";
 import cors from "cors";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger-output.json" with {type: "json"};
+import swaggerFile from "./swagger-output.json" assert {type: "json"};
 
 const app = express();
 const porta = process.env.PORT || 3000;
