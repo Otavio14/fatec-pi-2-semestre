a33aa1c66ffea46da70988a3b761a8e86d259bef
OtavioBScar <otavio.scarpelline@gmail.com>
2024-11-02 19:01:48 -0300

    feat: modal de endereço

--- frontend/src/components/endereco-modal.jsx
+++ frontend/src/components/endereco-modal.jsx
@@ -3,12 +3,8 @@ import { Input } from "./input";
 
 export const EnderecoModal = ({ show, setShow, cep, bairro, rua, numero }) => {
 
-    if (show == false) {
-        return
-    }
-
     return (
-        <div className="flex fixed inset-0 justify-center items-center bg-black bg-opacity-50">
+        <div className={`${show == true ? "flex fixed inset-0 justify-center items-center bg-black bg-opacity-50" : "hidden"} `}>
             <div className="flex flex-col w-[25%] bg-white border-2 border-black p-10 rounded-2xl">
                 <button className="border-2 border-black rounded-full px-2 w-fit hover:bg-red-700" onClick={() => setShow(!show)}>X</button>
                 <Input Label={"CEP"} value={cep} />
